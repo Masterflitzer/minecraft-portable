61ae6211d10da7b1967581fb04ae098f5e740908
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2020-02-18 15:31:48 +0100

    Update minecraft.cmd

--- minecraft.cmd
+++ minecraft.cmd
@@ -1,8 +1,7 @@
 @echo off
+cd %~dp0
 if not exist "%tmp%\.minecraft" mkdir "%tmp%\.minecraft"
-robocopy ".\.minecraft" "%tmp%\.minecraft" -mir -mt
-pause
-".\MinecraftLauncher.exe" --workDir "%tmp%\.minecraft" --tmpDir "%tmp%" --lockDir ".\.minecraft"
-pause
 if not exist ".\.minecraft" mkdir ".\.minecraft"
+robocopy ".\.minecraft" "%tmp%\.minecraft" -mir -mt
+".\MinecraftLauncher.exe" --workDir "%tmp%\.minecraft" --tmpDir "%tmp%" --lockDir ".\.minecraft"
 robocopy "%tmp%\.minecraft" ".\.minecraft" -mir -mt
