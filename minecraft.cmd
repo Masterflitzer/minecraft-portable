6bd7496e2da3011683fd40c40e5e75dd61da5906
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2020-02-18 18:18:55 +0100

    Delete minecraft.cmd

--- minecraft.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd %~dp0
-if not exist "%tmp%\.minecraft" mkdir "%tmp%\.minecraft"
-if not exist ".\.minecraft" mkdir ".\.minecraft"
-robocopy ".\.minecraft" "%tmp%\.minecraft" -mir -mt
-".\MinecraftLauncher.exe" --workDir "%tmp%\.minecraft" --tmpDir "%tmp%" --lockDir ".\.minecraft"
-robocopy "%tmp%\.minecraft" ".\.minecraft" -mir -mt
